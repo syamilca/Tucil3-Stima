d1312b1cf454d45713b5236b714055a76aeb8e57
hafid abi <abi.daniswara@gmail.com>
2021-04-05 23:15:33 +0700

    astar masih error

--- src/tesclass.js
+++ src/tesclass.js
@@ -1,12 +1,17 @@
 class Node {
-    constructor(value){
-        this.value = value;
+    constructor(value,lat,long){
+        this.value = value.trim();
+        this.long = long
+        this.lat = lat
         this.numOfFriend = 0;
         this.friends = [];
     }
-    addFriend(value, weight){
+    addFriend(value, lat, long){
         if (!this.isFriend(value)){
-            this.friends.push([value, weight]);
+            this.friends.push({
+                value : value,
+                jarak : this.getHaversine({lat : this.lat, long: this.long},{lat: lat, long: long})
+            });
             this.numOfFriend++;
         }
     }
@@ -18,15 +23,41 @@ class Node {
     }
     searchFriend(value){
         for (let i = 0; i < this.numOfFriend; i++){
-            if (this.friends[i][0] == value) return i;
+            if (this.friends[i].value == value) return i;
         }
         return -1;
     }
     getWeight(value){
         if (this.isFriend(value)){
-            return this.friends[this.searchFriend(value)][1];
+            return this.friends[this.searchFriend(value)].jarak;
         }
         else return -1;
+    }
+    getHaversine(koordinat1, koordinat2){
+        //KOORDINAT1 & KOORDINAT2 DALAM FROMAT
+        /**
+         * {
+         * lat : 1.234,
+         * long : 1.123 
+         * }
+         */
+        let x1 = koordinat2.lat -koordinat1.lat
+        let dLat = x1 *Math.PI /180
+        let x2 = koordinat2.long -koordinat1.long
+        let dLong = x2 * Math.PI/180
+        let temp1 = (Math.sin(dLat/2) * Math.sin(dLat/2)) + (Math.cos(koordinat1.lat *Math.PI /180) * Math.cos(koordinat2.lat * Math.PI /180) * Math.sin(dLong/2) * Math.sin(dLong/2))
+        let temp2 = 2* Math.atan2(Math.sqrt(temp1), Math.sqrt(1-temp1))
+        return temp2*6371000
+    }
+    deleteFriend(val){
+        let newf = []
+        this.friends.forEach((i)=>{
+            if(i.value!=val){
+                newf.push(i)
+            }
+        })
+        this.numOfFriend--;
+        this.friends = newf
     }
 };
 
@@ -41,145 +72,85 @@ class Graph {
     }
     searchNode(val){
         for (let i = 0; i < this.numOfNodes; i++){
-            if (this.nodes[i].value == val) return i;
+            if (this.nodes[i].value === val) return i;
         }
         return -1;
     }
-    addNode(val, friend, weight){
+    addNode(val, nodeLat, nodeLong){
         if (!this.isExist(val)){
-            let newNode = new Node(val);
-            newNode.addFriend(friend, weight);
-            this.nodes.push(newNode);
+            let newNode = new Node(val, nodeLat, nodeLong)
+            this.nodes.push(newNode)
             this.numOfNodes++;
-        } else {
-            let idx = this.searchNode(val);
-            if (!this.nodes[idx].isFriend(friend)){
-                this.nodes[idx].addFriend(friend, weight);
-            }
         }
-        if (!this.isExist(friend)){
-            let newNode = new Node(friend);
-            newNode.addFriend(val, weight);
-            this.nodes.push(newNode);
-            this.numOfNodes++;
-        } else {
-            let idx = this.searchNode(friend);
-            if (!this.nodes[idx].isFriend(val)){
-                this.nodes[idx].addFriend(val, weight);
-            }
+    }
+    addFriend(nodeId, friendId){
+        if(this.isExist(nodeId) && this.isExist(friendId)){
+            let i = this.searchNode(nodeId)
+            let j = this.searchNode(friendId)
+            this.nodes[i].addFriend(this.nodes[j].value, this.nodes[j].lat, this.nodes[j].long)
         }
     }
     getNodebyValue(val){
         return this.nodes[this.searchNode(val)];
     }
-    createAdjMatrix(){
-        let adjMatrix = [];
-        for(let i=0; i<this.numOfNodes; i++) {
-            adjMatrix[i] = [];
+    getNodebyIndex(index){
+        if(index>=0 && index<this.numOfNodes){
+            return this.nodes[index]
         }
-        for (let i = 0; i < this.numOfNodes; i++){
-            for (let j = 0; j < this.numOfNodes; j++){
-                if (this.nodes[i].isFriend(this.nodes[j].value)){
-                    adjMatrix[i].push(1);
-                } else{
-                    adjMatrix[i].push(0);
-                }
-            }
-        }
-        return adjMatrix;
+        return 0
     }
-    printInfo(){
-        for (let i = 0; i < this.numOfNodes; i++){
-            console.log("Node value : " + this.nodes[i].value);
-            console.log("Node friends : ");
-            for (let j = 0; j < this.nodes[i].numOfFriend; j++){
-                console.log(this.nodes[i].friends[j][0] + ", weight : " + this.nodes[i].friends[j][1]);
-            }
+    getHeuristicArray(dest){
+        let res = []
+        if(this.isExist(dest)){
+            let dnode = this.getNodebyValue(dest)
+            this.nodes.forEach((node)=>{
+                res.push(this.getHaversine({lat:node.lat,long:node.long},{lat:dnode.lat,long:dnode.long}))
+            })
         }
-        let nodeString = "";
-        for (let i = 0; i < this.numOfNodes; i++){
-            nodeString += "  " + this.nodes[i].value;
-        }
-        let adjacentMatrix = this.createAdjMatrix();
-        console.log("Adjacent matrix : ");
-        console.log(nodeString);
-        for (let i = 0; i < this.numOfNodes; i++){
-            console.log(this.nodes[i].value + " " + adjacentMatrix[i].join("  "));
-        }
+        return res
     }
-    BFS(node1, node2){
-        let antrian = [];
-        let dikunjungi = Array(this.numOfNodes).fill(false);
-        let kunjungan = [];
-        var currNode;
-        antrian.push(node1);
-        while (antrian.length != 0 && currNode != node2){
-            currNode = antrian.shift();
-            kunjungan.push(currNode);
-            dikunjungi[this.searchNode(currNode.value)] = true;
-            let currFriend = currNode.friends;
-            for (let i = 0; i < currNode.numOfFriend; i++){
-                if (dikunjungi[this.searchNode(currFriend[i][0])] != true && !antrian.includes(this.getNodebyValue(currFriend[i][0]))){
-                    antrian.push(this.getNodebyValue(currFriend[i][0]))
-                }
-            }
-        }
-        if (kunjungan.includes(node2)){
-            //console.log(kunjungan);
-            for (let i = kunjungan.length - 1; i >= 1; i--){
-                //console.log(kunjungan[i]);
-                if (!kunjungan[i].isFriend(kunjungan[i-1].value)){
-                    //delete kunjungan[i-1];
-                    for (let j = i-1; j < kunjungan.length-1; j++){
-                        kunjungan[j] = kunjungan[j+1];
-                    }
-                    let dump = kunjungan.pop();
-                }
-            }
-            for (let i = kunjungan.length - 1; i >= 2; i--){
-                if (kunjungan[i].isFriend(kunjungan[i-2].value)){
-                    let weight1 = kunjungan[i].getWeight(kunjungan[i-1].value);
-                    let weight2 = kunjungan[i-1].getWeight(kunjungan[i-2].value);
-                    let weight3 = kunjungan[i].getWeight(kunjungan[i-2].value);
-                    if (weight1 + weight2 > weight3){
-                        for (let j = i-1; j < kunjungan.length-1; j++){
-                            kunjungan[j] = kunjungan[j+1];
+    getHaversine(koordinat1, koordinat2){
+        //KOORDINAT1 & KOORDINAT2 DALAM FROMAT
+        /**
+         * {
+         * lat : 1.234,
+         * long : 1.123 
+         * }
+         */
+        let x1 = koordinat2.lat -koordinat1.lat
+        let dLat = x1 *Math.PI /180
+        let x2 = koordinat2.long -koordinat1.long
+        let dLong = x2 * Math.PI/180
+        let temp1 = (Math.sin(dLat/2) * Math.sin(dLat/2)) + (Math.cos(koordinat1.lat *Math.PI /180) * Math.cos(koordinat2.lat * Math.PI /180) * Math.sin(dLong/2) * Math.sin(dLong/2))
+        let temp2 = 2* Math.atan2(Math.sqrt(temp1), Math.sqrt(1-temp1))
+        return temp2*6371000
+    }
+    getLineOneAnother(){
+        let res = []
+        for (let x=0;x<this.numOfNodes;x++){
+            for(let y=1;y<this.numOfNodes ; y++){
+                const n1 = this.getNodebyIndex(x)
+                const n2 = this.getNodebyIndex(y)
+                const dist = this.getHaversine({lat: n1.lat, long:n1.long},{lat: n2.lat, long:n2.long}).toFixed(2)
+                if(n1.isFriend(n2.value)){
+                    res.push({
+                        'type' : 'Feature',
+                        'geometry' : {
+                            'type' : 'LineString',
+                            'coordinates' : [
+                                [n1.long, n1.lat],
+                                [n2.long, n2.lat]
+                            ]
+                        },
+                        "properties" : {
+                            "title" : dist+" m"
                         }
-                        let dump = kunjungan.pop();
-                    }
+                    })
                 }
             }
-            //console.log(kunjungan);
-            return kunjungan;
-        } else {
-            let dump = [];
-            return dump;
         }
-    }
-    countPredict(node1, node2){
-        let temp = this.BFS(node1, node2);
-        let sum = 0;
-        for (let i = 0; i < temp.length-1; i++){
-            sum += temp[i].getWeight(temp[i+1].value);
-        }
-        return sum;
+        return res
     }
 };
 
-let Graf = new Graph();
-Graf.addNode("A", "B", 3);
-Graf.addNode("A", "C", 5);
-Graf.addNode("A", "D", 2);
-Graf.addNode("B", "C", 7);
-Graf.addNode("B", "E", 2);
-Graf.addNode("B", "F", 3);
-Graf.addNode("C", "F", 2);
-Graf.addNode("C", "G", 1);
-Graf.addNode("D", "G", 3);
-Graf.addNode("D", "F", 4);
-Graf.addNode("E", "H", 5);
-Graf.addNode("E", "F", 2);
-Graf.addNode("F", "H", 1);
-//console.log(Graf.nodes);
-console.log(Graf.countPredict(Graf.getNodebyValue("A"), Graf.getNodebyValue("H")));
-//Graf.printInfo();
+
--- src/tucil2.js
+++ src/tucil2.js
@@ -1,115 +1,336 @@
-var script = document.createElement('script')
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDOWsPALCtSWl077QszEVy8OUeg7MEoi34"
-script.async = true
-document.head.appendChild(script)
-
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3BlY2lhbG9uZTE2IiwiYSI6ImNramN0enVlZzJjMDYycXA5bXJ1OXE2bTcifQ.aTWG4bxIEgemZu-9xJ9i6w'
 let dept = ""
 let dest = ""
-let graf = {
-    getNode : function(nodename){
-
-    },
-    getJarak : function(node1, node2){
-
-    },
-    getHeuristik : function(nodeStart, nodeFinish){
-
-    }
-}
-document.getElementById('inputfile').addEventListener('change', function() 
-    {
-        const fr=new FileReader();
-        dept = ""
-        dest = ""
-        fr.onload=  () => bacaTxt(fr.result);
-        fr.readAsText(this.files[0]);
-    })
+let myGraf = new Graph()
+let directionAddedFlag = false
+let myMap
 
 function bacaTxt(result){
     let temp = []
     temp.push.apply(temp,result.split('\n'));
-    if(Number(temp[0])!=NaN && temp.length == (2*Number(temp[0]))+1){
-        graf.jumlahNode = Number(temp[0])
-        graf.nodes = []
+    if(!isNaN(temp[0]) && temp.length == (2*Number(temp[0]))+1){
+        let nNode = Number(temp[0])
         
-        var i,j
-        for(i=1; i<=graf.jumlahNode; i++){
+        let i
+        for(i=1; i<=nNode; i++){
             let nodeTemp = temp[i].split(" ")
-            graf.nodes.push({
-                lat : nodeTemp[0],
-                long : nodeTemp[1],
-                name : nodeTemp[2],
-                adjacency : []
+            myGraf.addNode(String(nodeTemp[2]),nodeTemp[0],nodeTemp[1])
+        }
+        for(i=1+nNode;i<=2*nNode;i++){
+            const t = temp[i].split(' ')
+            if(t.length!=nNode) throw "matriks ketetanggaan harus matriks persegi"
+            let counter = 0
+            t.forEach((isFriend)=>{
+                if(isNaN(isFriend) || !(Number(isFriend)<=1 && Number(isFriend)>=0)){
+                    throw "matriks should be in number 1 and 0 only"
+                }
+                if(Number(isFriend)==1){
+                    myGraf.addFriend(myGraf.getNodebyIndex(i-1-nNode).value, myGraf.getNodebyIndex(counter).value)
+                }
+                counter++
             })
         }
-        for(i=1+graf.jumlahNode;i<=2*graf.jumlahNode;i++){
-            const t = temp[i].split(' ')
-            for(j=0;j<graf.jumlahNode;j++){
-                if(t[j]=='1'){
-                    graf.nodes[i-1-graf.jumlahNode].adjacency.push(graf.nodes[j].name)
-                }
-            }
-        }
         setComboBox()
-        muatPeta()
+        muatPeta2()
         //document.getElementById("output").textContent = "ACC"
     }else{
-        document.getElementById("output").textContent = "ERROR"
+        throw "error. cek format testcase sesuai dengan readme!"
     }
-    console.log(graf)
+    console.log(myGraf)
 }
 
 function klik(){
-    console.log(graf)
+    if(dept===dest){
+        console.log("error, depature harus != destination")
+    }else{
+        try{
+            const grafduplikat = myGraf
+            haha = a_star(grafduplikat,dept,dest)
+            setDirectionOnMap(haha.rute,dept,dest)
+            console.log(haha)
+            document.getElementById("output").innerHTML = haha.rute+"<br>"+haha.heuristik+"<br>jarak = "+haha.jarak
+        }catch(err){
+            document.getElementById("output").textContent = err
+            console.log(err)
+        }
+    }
 }
 
-function muatPeta(){
-    //console.log(graf)
-    var mapprops = {
-        center:new google.maps.LatLng(graf.nodes[0].lat,graf.nodes[0].long),
-        zoom:15,
-        mapTypeId:'roadmap'
-    };
-    const myMap = new google.maps.Map(document.getElementById("googleMap"),mapprops)
+function muatPeta2(){
+    myMap = new mapboxgl.Map(
+        {
+            container : 'googleMap',
+            style: 'mapbox://styles/mapbox/streets-v9', // style URL
+            center: [Number(myGraf.nodes[0].long),Number(myGraf.nodes[0].lat)], // starting position as [lng, lat]
+            zoom: 14
+        }
+    )
 
-    var i
-    for(i=0;i<graf.jumlahNode;i++){
-        new google.maps.Marker({
-            position : new google.maps.LatLng(graf.nodes[i].lat,graf.nodes[i].long),
-            map : myMap,
-            title : graf.nodes[i].name
-        })
+    let lokasi = {
+        'type' : 'FeatureCollection',
+        'features' : []
     }
+    for(let i =0; i<myGraf.nodes.length;i++){
+        const myNode = myGraf.getNodebyIndex(i)
+        lokasi.features.push(
+            {
+                'type': 'Feature',
+                'properties': {
+                'description': myNode.value,
+                },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [myNode.long, myNode.lat]
+                }
+            }
+        )
+    }
+
+    myMap.on('load',function(){
+        const mapLine = myGraf.getLineOneAnother()
+        let counter = 1
+        mapLine.forEach((dataKoordinat) => {
+            const n = 'garis'+counter
+            myMap.addSource(n, {
+                'type' : 'geojson',
+                'data' : dataKoordinat
+            })
+
+            myMap.addLayer({
+                'id': n,
+                'type': 'line',
+                'source': n,
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round',
+                },
+                'paint': {
+                    'line-color': '#888',
+                    'line-width': 3
+                }
+            })
+
+            myMap.addLayer({
+                "id": "jarak2titik-"+counter,
+                "type": "symbol",
+                "source": n,
+                "layout": {
+                  "symbol-placement": "line-center",
+                  "text-font": ["Open Sans Regular"],
+                  "text-field": '{title}',
+                  "text-size": 13,
+                  "text-rotate": -4,
+                  "symbol-spacing": 1,
+                },
+                "paint":{
+                  "text-translate":[0,-40],
+                }
+              })
+            counter++
+        })
+
+        myMap.addSource('directions',{
+            'type' : 'geojson',
+            'data' : {
+                'type' : 'Feature',
+                'geometry' :{
+                    'type' :'LineString', 
+                    'coordinates' : []
+                } ,
+                
+            }
+        })
+
+        myMap.addLayer({
+            'id': 'directions',
+            'source': 'directions',
+            'type': 'line',
+            'paint': {
+                'line-width': 4,
+                'line-color': '#f013b1'
+            }
+        })
+
+        myMap.addSource('places', {
+            'type': 'geojson',
+            'data': lokasi
+            });
+
+        myMap.addLayer({
+            'id': 'poi-labels',
+            'type': 'symbol',
+            'source': 'places',
+            'layout': {
+                'text-field': ['get', 'description'],
+                'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+                'text-radial-offset': 0.5,
+                'text-justify': 'auto',
+                'icon-image': ['concat', ['get', 'icon'], '-15']
+            },
+            'paint' : {
+                'text-color' : '#556fe0'
+            }
+        });
+    })
+
+
+
 }
 
 function setComboBox(){
     let depature = '<option value="0">Select Depature Point</option>'
     let i
-    for(i=0;i<graf.jumlahNode;i++){
-        depature = depature + '<option value="'+ graf.nodes[i].name +'">'+ graf.nodes[i].name +'</option>'
+    for(i=0;i<myGraf.nodes.length;i++){
+        depature = depature + '<option value="'+myGraf.nodes[i].value+'">'+ myGraf.nodes[i].value +'</option>'
     }
     document.getElementById("depatureNode").innerHTML = depature
 }
 
+function setDirectionOnMap(listOfPassedNodes, start, end){
+    let dir = {
+        'type' : 'Feature',
+        'geometry' :{
+            'type' :'LineString', 
+            'coordinates' : []
+        }
+    }
+
+    listOfPassedNodes.forEach((node)=>{
+        dir.geometry.coordinates.push(
+            [myGraf.getNodebyValue(node).long,myGraf.getNodebyValue(node).lat]
+        )
+    })
+    myMap.getSource('directions').setData(dir)
+}
+
 document.getElementById("depatureNode").addEventListener("change",function(){
+    document.getElementById("destinationNode").innerHTML = ""
     if(this.value!='0'){
-        dept = this.value
+        dept = String(this.value).replace('\n','').trim()
         dest = ""
         let destination = '<option value="0">Select Destination</option>'
-        let i
-        for(i=0;i<graf.jumlahNode;i++){
-            if(this.value!=graf.nodes[i].name){
-                destination = destination + '<option value="'+ graf.nodes[i].name +'">'+ graf.nodes[i].name +'</option>'
+        for(let i=0;i<myGraf.nodes.length;i++){
+            if(dept!=(myGraf.nodes[i].value.trim())!=0){
+                destination = destination + '<option value="'+myGraf.nodes[i].value+'">'+myGraf.nodes[i].value+'</option>'
             }
         }
         document.getElementById("destinationNode").innerHTML = destination
-    }else{
-        document.getElementById("destinationNode").innerHTML = ""
     }
 },false)
 
-document.getElementById("destinationNode").addEventListener("change",function(){
+document.getElementById("destinationNode").addEventListener("change",function(){ 
     if(this.value!='0'){
-        dest = this.value
+        dest = String(this.value).replace('\n','').trim()
     }
 },false)
+
+document.getElementById('inputfile').addEventListener('change', function() 
+    {
+        const fr=new FileReader();
+        dept = ""
+        dest = ""
+        fr.onload=  () => {
+            try{
+                bacaTxt(fr.result)
+            }catch(err){
+                document.getElementById("output").textContent = err
+            }
+        };
+        fr.readAsText(this.files[0]);
+        
+})
+
+/**
+ * 
+ * @param {Graph} graf 
+ * @param {string} start 
+ * @param {string} destination 
+ */
+ function a_star(graf,start,destination){
+    let distanceResult = 0
+    let route = [start]
+    let bannedNode = []
+    let isSuccess = false
+    let isFail  = false
+    let heuristik = graf.getHeuristicArray(destination)
+    //CHECK START AND DESTINATION ARE ON GRAF
+    if(!graf.isExist(start)||!graf.isExist(destination)) throw "node tujuan/awalan tidak pernah ada disistem!"
+    //DOIN A_STAR
+    while(!isSuccess && !isFail){
+        let tempNode = graf.getNodebyValue(start)
+        let flag = true
+        while(flag){
+            //CEK TETANGGA DAN HAPUS TETANGGA YANG SUDAH ADA PADA ROUTE or BANNED NODE
+            route.forEach((r)=>{tempNode.deleteFriend(r)})
+            bannedNode.forEach((r)=>{tempNode.deleteFriend(r)})
+
+            //CEK APAKAH ADA TETANGGA MINIMAL 1
+            if(tempNode.friends.length>0)
+            //JIKA ADA CARI TERPENDEK
+            {
+                let tNode2 = graf.getNodebyValue(tempNode.friends[0].value)
+                let tJarak =  tempNode.friends[0].jarak + heuristik[graf.searchNode(tempNode.friends[0].value)]
+                for(let x = 1; x<tempNode.friends.length;x++){
+                    if(tempNode.friends[x].value==destination){
+                        tJarak = tempNode.friends[x].jarak + heuristik[graf.searchNode(tempNode.friends[x].value)]
+                        tNode2 = graf.getNodebyValue(tempNode.friends[x].value)
+                        break
+                    }else if(tempNode.friends[x].jarak + heuristik[graf.searchNode(tempNode.friends[x].value)] < tJarak){
+                        tJarak = tempNode.friends[x].jarak + heuristik[graf.searchNode(tempNode.friends[x].value)]
+                        tNode2 = graf.getNodebyValue(tempNode.friends[x].value)
+                    }
+                }
+                distanceResult = distanceResult + tJarak
+                console.log(distanceResult)
+                route.push(tNode2.value)
+                if(tNode2.value===destination){
+                    isSuccess=true
+                    flag = false
+                }else{
+                    tempNode = tNode2
+                }
+
+            }else
+            //JIKA TIDAK ADA 
+            {
+                flag = false
+                if(route.length<=1){
+                    isFail = true
+                }else{
+                    bannedNode.push(tempNode.value)
+                    tempNode = graf.getNodebyValue(route[route.length-2])
+                    distanceResult = distanceResult - graf.getNodebyValue(route[route.length-2]).getWeight(route[route.length-1])
+                    route = deleteArray(route,route[route.length-1])
+                }
+            }
+        }
+    }
+
+    if(isSuccess){
+        return {
+            'rute' : route,
+            'jarak' : distanceResult,
+            'heuristik' : heuristik
+        }
+    }else{
+        throw "gagal melakukan A*, coba cek graf anda sekali lagi  :)"
+    }
+   
+
+}
+
+/**
+ * 
+ * @param {Array} oldArray 
+ * @param {*} key 
+ */
+function deleteArray(oldArray, key){
+    let newArray = []
+    oldArray.forEach((i)=>{
+        if(i!=key){
+            newArray.push(i)
+        }
+    })
+    return newArray
+}
+
